befaf90ceeb5617a1f20219b226fb0f84e567936
Alex Nichol <unixpickle@gmail.com>
2014-03-30 14:12:36 -0400

    added missing include

---
+++ include/anmem/alloc.h
@@ -0,0 +1,24 @@
+#include "types.h"
+
+/**
+ * Allocate `len` physical pages, aligned to a `len` pages boundary.
+ * @return If no region could be allocated (either because of alignment or
+ * space issues) NULL is returned. Otherwise, a pointer is returned. The
+ * pointer is in virtual space.
+ */
+void * anmem_alloc_aligned(anmem_t * mem, uint64_t len);
+
+/**
+ * Free an aligned physical buffer of a certain length in virtual space.
+ */
+void anmem_free_aligned(anmem_t * mem, void * buffer, uint64_t len);
+
+/**
+ * Allocate a page (aligned) segment of memory from the fastest source.
+ */
+void * anmem_alloc_page();
+
+/**
+ * Free a page of memory which was allocated with anmem_alloc_page().
+ */
+void anmem_free_page(anmem_t * mem, void * page);
